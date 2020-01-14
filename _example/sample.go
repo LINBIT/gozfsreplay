c53576547102f196efcdc1a6fca8b71e49eaca73
Roland Kammerer <roland.kammerer@linbit.com>
2020-01-14 13:54:30 +0100

    add example

---
+++ _example/sample.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/LINBIT/zfsreplay"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal(os.Args[0], "file.zstream")
+	}
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+Loop:
+	for {
+		obj, typ, err := zfsreplay.DRRRead(f, binary.LittleEndian)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch typ {
+		case zfsreplay.DRR_BEGIN:
+			_ = obj.(zfsreplay.DRR_begin)
+			fmt.Println("begin")
+		case zfsreplay.DRR_FREEOBJECTS:
+			_ = obj.(zfsreplay.DRR_freeobjects)
+			fmt.Println("freeobjects")
+		case zfsreplay.DRR_OBJECT:
+			_ = obj.(zfsreplay.DRR_object)
+			fmt.Println("object")
+		case zfsreplay.DRR_FREE:
+			_ = obj.(zfsreplay.DRR_free)
+			fmt.Println("free")
+		case zfsreplay.DRR_WRITE:
+			_ = obj.(zfsreplay.DRR_write)
+			fmt.Println("write")
+		case zfsreplay.DRR_END:
+			_ = obj.(zfsreplay.DRR_end)
+			fmt.Println("end")
+			break Loop
+		default:
+			fmt.Println("DEFAULT main")
+			break Loop
+		}
+	}
+}
